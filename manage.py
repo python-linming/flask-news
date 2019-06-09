a6d30e54938abcc7ccaff681d625de3e89baeff0
python-linming <765800916@qq.com>
2019-06-09 11:29:23 +0800

    集成manager

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -38,6 +39,8 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+Migrate(app, db)  # 关联app ,db
+manager.add_command('db', MigrateCommand)  # 迁移命令
 
 
 @app.route('/index')
