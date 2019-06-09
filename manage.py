751a9157a13ecfc4f24004d0c497d3a13aadd1d0
python-linming <765800916@qq.com>
2019-06-09 11:34:52 +0800

    抽取

--- manage.py
+++ manage.py
@@ -5,29 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
-
-class Config(object):
-    """项目配置"""
-
-    DEBUG = True
-    SECRETY_KEY = 'sdfsfsdfds'
-    # mysql
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/news'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # session
-    SESSION_TYPE = 'redis'
-    # 指定session保存到redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    SESSION_USE_SIGNER = True  # 数字签名
-    SESSION_PERMANENT = False  # 设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
